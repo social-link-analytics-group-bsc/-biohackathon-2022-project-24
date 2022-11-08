9890ca4e85a5d9ac71b4913b8d934f0f13a01e71
Oliph <olivier.philippe@gmail.com>
2022-11-08 17:12:44 +0100

    Add annotation filter

--- pipeline/use_api.py
+++ pipeline/use_api.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import requests
 from requests.exceptions import HTTPError
 
@@ -6,13 +5,12 @@ import xml.etree.ElementTree as ET
 import logging
 import os
 import yaml
-import sys
-
+from tqdm import tqdm
 import pathlib
+import urllib.parse
+from urllib.parse import quote
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from db_utils import db_utils
 logger = logging.getLogger(__name__)
 
 
@@ -31,56 +29,8 @@ def get_request(url, params):
     except Exception as err:
         print(f'Other error occurred: {err}')
     else:
-        return requests.get(url, params)
+        return response
 
-
-def retrieveSupplementary(root):
-    for supplementary in root.iter('supplementary-material'):
-        return str(ET.tostring(supplementary)).replace('"', "'")
-
-
-def retrieveMetadata(root):
-    dictMetadata = {
-        'issn': {'ppub': '', 'epub': ''},
-        'journalTitle': '',
-        'publisherName': ''
-    }
-    for front in root.iter('front'):
-        for journalId in front.iter('issn'):
-            if "ppub" in journalId.attrib['pub-type']:
-                dictMetadata['issn']['ppub'] = ''.join(journalId.itertext())
-            if "epub" in journalId.attrib['pub-type']:
-                dictMetadata['issn']['epub'] = ''.join(journalId.itertext())
-        for journalTitle in front.iter('journal-title'):
-            dictMetadata['journalTitle'] = ''.join(journalTitle.itertext())
-        for publisher in front.iter('publisher-name'):
-            dictMetadata['publisherName'] = ''.join(publisher.itertext())
-    return dictMetadata
-
-
-# Retrieve the text for each section
-def retrieveSections(root):
-
-    dictSection = {'Intro': '', 'Method': '', 'Result': '', 'Discussion': ''}
-    for body in root.iter('body'):
-        for child in body.iter('sec'):
-            if "sec-type" in child.attrib:
-                for section in dictSection.keys():  # For each section in dictSection
-                    # Sections inside sec-type are written in the following form:
-                    # intro, methods, results, discussion
-                    if section.lower() in child.attrib["sec-type"].lower():
-                        dictSection[section] = ''.join(child.itertext()).replace(
-                            '"', "'")  # Text without tags
-                        # dictSection[section] = ET.tostring(child) # Text with tags
-            if child[0].text:
-                for section, sectionData in dictSection.items():
-                    if sectionData:
-                        continue
-                    if section.lower() in child[0].text.lower():
-                        dictSection[section] = ''.join(child.itertext()).replace(
-                            '"', "'")  # Text without tags
-                        # dictSection[section] = ET.tostring(child) # Text with tags
-    return dictSection
 
 
 def apiSearch(pmcid, root_url):
@@ -93,7 +43,29 @@ def apiSearch(pmcid, root_url):
         return
     return root
 
-# load pmcid from csv file
+
+def get_species(pmcid, annotation_url, accepted_species):
+    
+    url = annotation_url
+    pmcid = f'PMC:{pmcid[3:]}'
+    payload = {'articleIds': pmcid,
+              'type': 'Organisms',
+              'section': 'Methods', 
+              'provider': 'Europe PMC',
+              # 'provider': requests.utils.quote('Europe PMC'),
+              'format': 'JSON'}
+
+    params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
+    result = get_request(url, params)
+    result_json = result.json()
+    species = None
+    for dictionary in result_json:
+        for d in dictionary['annotations']:
+            species = d['exact']
+            break
+    if species in accepted_species:
+        return pmcid
+
 
 
 def getPMCidList(file_location):
@@ -110,21 +82,10 @@ def getPMCidList(file_location):
 
 def main():
     api_root_article = config_all['api_europepmc_params']['rest_articles']['root_url']
-    api_root_archive = config_all['api_europepmc_params']['archive_api']['root_url']
-    file_root_archive = config_all['api_europepmc_params']['archive_file']
-    rerun_archive = config_all['api_europepmc_params']['rerun_archive']
-    file_location = config_all['ids_file_location']
+    annotation_api = config_all['api_europepmc_params']['annotations_api']['root_url']
+    file_location = config_all['search_params']['ids_file_location']
     article_folder = config_all['api_europepmc_params']['article_folder']
-    # if rerun_archive is True:
-    #   db_utils.drop_database()
-    # db_utils.create_database()
-
-    # Get the present pcmid in case rerun=false to avoir re-dl everything
-    # already_dl_pcmid = [i for i in db_utils.retrieve_existing_record()]
-
-    dummyCounter = 0
-    # archive = get_archive(file_root_archive, api_root_archive, rerun_archive)
-
+    accepted_species = config_all['search_params']['accepted_species']
     pmcid_to_dl = list(getPMCidList(file_location))
     print(f"Len of pmcid_to_dl: {len(pmcid_to_dl)}")
     already_dl_pmcid = list()
@@ -134,17 +95,13 @@ def main():
     list_pmcid = [pmcid for pmcid in pmcid_to_dl if pmcid not in already_dl_pmcid]
 
     print(f"Len list_pmcid: {len(list_pmcid)}")
-    for pmcid in list_pmcid:
+    for pmcid in tqdm(list_pmcid):
         print(pmcid)
-        dummyCounter += 1
-        article = apiSearch(pmcid, api_root_article)
-        try:
+        if get_species(pmcid, annotation_api, accepted_species):
+            article = apiSearch(pmcid, api_root_article)
             tree = ET.ElementTree(article)
             with open('data/articles/'+pmcid+'.xml', 'wb') as f:
                 tree.write(f)
-        except AttributeError:
-            print('no output')
-
 
 if __name__ == "__main__":
     main()
