20ab8a67c563f000b57a315706e867fc2a5b76dc
Othmane Hayoun Mya <74412118+othmya@users.noreply.github.com>
2022-11-09 16:38:37 +0100

    small edits added

--- pipeline/explore_methods.py
+++ pipeline/explore_methods.py
@@ -1,3 +1,4 @@
+from ast import Break
 import json
 import glob
 import nltk
@@ -5,8 +6,9 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 #nltk.download('punkt') # uncomment the first time you run the script
 import csv
 import os
+import pandas as pd
 
-def find_candidate_sentences(text, relevant_tokens):
+def find_candidate_sentences(text, relevant_tokens, get_null_sentences=False):
     sentences = sent_tokenize((text))
     interest_sentences = []
     for sent in sentences:
@@ -15,39 +17,62 @@ def find_candidate_sentences(text, relevant_tokens):
         tokenized = word_tokenize(sent)
         for i,token in enumerate(tokenized):
             if token.lower() in relevant_tokens:
-                try:
-                    check_tokens = tokenized[i-window:i+window]
-                except IndexError:
-                    try:
-                        check_tokens = tokenized[i-1:i+1]
-                    except IndexError:
-                        continue
+                check_tokens = tokenized[i-window:i+window]
                 for t in check_tokens:
                     if t.isnumeric():
                         candidate = True
         if candidate and len(sent) < 300:
             interest_sentences.append(sent)
+    
+    if get_null_sentences: # get also those IDs where there is no match
+        if len(interest_sentences) == 0:
+            return ["None"] # convert to NaN at df level
+
     return interest_sentences
+
 
 def main():
 
     count_articles = 0
     interesting_tokens = ['man', 'woman', 'male', 'female', 'men', 'women', 'males', 'females']
+    header = ["PMCID", "tokenized_METHODS", "keywords"]
+
+
     open('data/candidate_sentences.csv', 'w')
 
+
     for file in glob.glob("data/clean_articles/"+"*.jsonl"):
-        print(file)
+        # print(file)
         with open(file, 'r') as o:
             data = json.load(o)
+            json_df = pd.read_json(file)
+            
         count_articles += 1
 
-        #text = data['METHODS']
-        sentences = find_candidate_sentences(data['METHODS'], interesting_tokens)
+        # filter JSON files that have no rows
+        if json_df.empty:
+            pass
+        if json_df.shape[0] > 1:
+            json_df_clean = json_df.loc[json_df.astype(str).drop_duplicates().index] 
+            json_df_clean = json_df_clean.iloc[0] # this is just a workaround
+            # json_df_clean.drop("TABLE", inplace=True)
+
+        sentences = find_candidate_sentences(data["METHODS"], interesting_tokens, get_null_sentences=True) 
 
         with open('data/candidate_sentences.csv', 'a') as o:
+            writer = csv.writer(o)
+            if count_articles == 1:
+                writer.writerow(header)
+            
             for sentence in sentences:
-                write = csv.writer(o)
-                write.writerow([os.path.basename(file[:-6]), sentence])
+                filename = os.path.basename(file[:-6]).split("archive_articles_tagged")[1]
+
+                try: # Only when "full" df is used
+                    writer.writerow([filename, sentence, json_df_clean["KEYWORDS"] ]) 
+                except:
+                    writer.writerow([filename, sentence, "None" ]) 
+    o.close()
+        
 
 if __name__ == "__main__":
     main()
