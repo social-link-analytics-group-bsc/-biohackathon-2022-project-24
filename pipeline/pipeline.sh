dcee8f152d65cbdc2428072e19a67e9f75a20bee
BlancaCalvo <blancacalvofigueras@gmail.com>
2022-11-10 17:57:10 +0100

    pipeline script

---
+++ pipeline/pipeline.sh
@@ -0,0 +1,15 @@
+
+XML_DATA_PATH = $1
+SENTENCES = "data/candidate_sentences.csv"
+MODEL = "train_model/model"
+RESULTS = "results.json"
+
+python explore_methods.py --data_path $XML_DATA_PATH --out $SENTENCES
+
+# TODO: more filtering script here
+
+python get_sex_bias.py --data $SENTENCES --out $RESULTS --model $MODEL
+
+# TODO: analysis scripts here
+
+
