fed73935d123c30e9afff5ac8a24f71e3ec0e023
Mouhamadou Ba <mba@jj-1404-port062-2.local>
2022-11-10 13:55:50 +0100

    adding annotation extraction

--- pipeline/more_filtering.py
+++ pipeline/more_filtering.py
@@ -1,7 +1,25 @@
+import requests
+from requests.exceptions import HTTPError
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import logging
+import os
+import json
+import yaml
+from tqdm import tqdm
+import pathlib
+import urllib.parse
+
 import pandas as pd
 import argparse
 from lxml import etree as ET
 """ script to add more info """
+
+
+config_path = os.path.join(os.path.dirname(
+    __file__), '../config', 'config.yaml')
+config_all = yaml.safe_load(open(config_path))
 
 
 def parse_xml(xmlString,id):
@@ -13,11 +31,55 @@ def parse_xml(xmlString,id):
 
     return article_dict
 
+def get_request(url, params):
+    try:
+        response = requests.get(url, params)
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        return response
+
+'''
+def getMetdata(pmcid, search_api_url, payload):
+    pmcid = f'PMC:{pmcid[3:]}'
+    params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
+    result = get_request(search_api_url, params)
+    result_json = result.json()
+'''
+
+def getAnnotations(pmcid, annotation_api_url, params):
+    extra_params = {'articleIds': f'PMC:{pmcid[3:]}', 'format': 'XML'}
+    new_p = dict(params)
+    new_p.update(extra_params)
+    result = get_request(annotation_api_url, new_p)
+    root = ET.fromstring(result.content)
+    return root
+
+def retrieveAnnotations(pmcid, annotation_api_url, params):
+    root = getAnnotations(pmcid, annotation_api_url, params)
+    annot_dict = {'PMCID': pmcid}
+    for k in params.keys():
+        l = []
+        for a in root.iter('annotation'):
+            if  a.find(k).text:
+                l.append(a.find('exact').text)
+        annot_dict[params[k]] = list(l)
+
+    return annot_dict
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This script will add more data to the dataframe')
     parser.add_argument("-f", "--file", nargs=1, required=True, help="Input csv", metavar="PATH")
     parser.add_argument("-d", "--directory", nargs=1, required=True, help="Directory in which xml files are stored", metavar="PATH")
     
+    annotation_api = config_all['api_europepmc_params']['annotations_api']['root_url']
+    params = {'type':'Diseases'}
+
     args = parser.parse_args()
     file = args.file[0]
     directory = args.directory[0]
@@ -34,6 +96,11 @@ if __name__ == "__main__":
         article_d.pop("Index")
         print(article_d)
         dic_of_dicts.append(article_d)
+
+        annotations = retrieveAnnotations(id, annotation_api, params)
+        print(annotations)
+        dic_of_dicts.append(annotations)
+
     df_new = pd.DataFrame(dic_of_dicts)
     print(df_new)
     df_new.to_csv("new_data.csv")
