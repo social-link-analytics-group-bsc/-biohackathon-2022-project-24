25c63408aab49ab56581c5f401a0ae40a67b5a99
BlancaCalvo <blancacalvofigueras@gmail.com>
2022-11-10 15:39:17 +0100

    reverted changed in explore_methods

--- pipeline/explore_methods.py
+++ pipeline/explore_methods.py
@@ -1,14 +1,12 @@
-import pandas as pd
 from ast import Break
 import json
-import pathlib
+import glob
+import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
+# nltk.download('punkt') # uncomment the first time you run the script
 import csv
 import os
-import yaml
-config_path = os.path.join(os.path.dirname(
-    __file__), '../config', 'config.yaml')
-config_all = yaml.safe_load(open(config_path))
+import pandas as pd
 
 
 def find_candidate_sentences(text, relevant_tokens, get_null_sentences=False):
@@ -20,7 +18,7 @@ def find_candidate_sentences(text, relevant_tokens, get_null_sentences=False):
         tokenized = word_tokenize(sent)
         for i, token in enumerate(tokenized):
             if token.lower() in relevant_tokens:
-                check_tokens = tokenized[i-window:i+window]
+                check_tokens = tokenized[i - window:i + window]
                 for t in check_tokens:
                     if t.isnumeric():
                         candidate = True
@@ -35,52 +33,43 @@ def find_candidate_sentences(text, relevant_tokens, get_null_sentences=False):
 
 
 def main():
-
-    dl_archive = config_all['search_params']['dl_archive']
-    article_query_folder = config_all['api_europepmc_params']['article_query_folder']
-
-    article_archive_folder = config_all['api_europepmc_params']['article_archive_folder']
-    candidate_sentences_location = config_all['processing_params']['candidate_sentence_location']
-
-    if dl_archive is True:
-        article_folder = article_archive_folder
-    else:
-        article_folder = article_query_folder
-
-    interesting_tokens = ['man', 'woman', 'male',
-                          'female', 'men', 'women', 'males', 'females']
+    count_articles = 0
+    interesting_tokens = ['man', 'woman', 'male', 'female', 'men', 'women', 'males', 'females']
     header = ["PMCID", "tokenized_METHODS", "keywords"]
 
-    open(candidate_sentences_location, 'w')
+    open('data/candidate_sentences.csv', 'w')
 
-    for filename in pathlib.Path(article_folder).glob('*.jsonl'):
-        pmcid = filename.stem
-        with open(filename, 'r') as o:
-            json_df = pd.read_json(filename)
+    for file in glob.glob("data/clean_articles/" + "*.jsonl"):
+        # print(file)
+        with open(file, 'r') as o:
+            data = json.load(o)
+            json_df = pd.read_json(file)
+
+        count_articles += 1
 
         # filter JSON files that have no rows
         if json_df.empty:
             pass
         if json_df.shape[0] > 1:
-            json_df_clean = json_df.loc[json_df.astype(
-                str).drop_duplicates().index]
+            json_df_clean = json_df.loc[json_df.astype(str).drop_duplicates().index]
             json_df_clean = json_df_clean.iloc[0]  # this is just a workaround
             # json_df_clean.drop("TABLE", inplace=True)
 
-        sentences = find_candidate_sentences(
-            json_df["METHODS"], interesting_tokens, get_null_sentences=True)
+        sentences = find_candidate_sentences(data["METHODS"], interesting_tokens, get_null_sentences=True)
 
-        with open(candidate_sentences_location, 'a') as o:
+        with open('data/candidate_sentences.csv', 'a') as o:
             writer = csv.writer(o)
-            writer.writerow(header)
+            if count_articles == 1:
+                writer.writerow(header)
 
             for sentence in sentences:
+                filename = os.path.basename(file[:-6]).split("archive_articles_tagged")[1]
+
                 try:  # Only when "full" df is used
-                    writer.writerow(
-                        [pmcid, sentence, json_df_clean["KEYWORDS"]])
+                    writer.writerow([filename, sentence, json_df_clean["KEYWORDS"]])
                 except:
-                    writer.writerow([pmcid, sentence, "None"])
-        o.close()
+                    writer.writerow([filename, sentence, "None"])
+    o.close()
 
 
 if __name__ == "__main__":
