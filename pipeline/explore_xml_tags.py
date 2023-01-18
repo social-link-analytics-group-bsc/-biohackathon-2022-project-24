ae2d973cd8cdb5cb0ab0e5acd9caddbde030321a
Olivier Philippe <olivier.philippe@gmail.com>
2023-01-18 14:59:24 +0100

    rewrite the way to extract info and extract sentences

--- pipeline/explore_xml_tags.py
+++ pipeline/explore_xml_tags.py
@@ -17,13 +17,16 @@ config_all = yaml.safe_load(open(config_path))
 logger = logging.getLogger(__name__)
 
 
-def parse_xml(tree, level_counts, tag_counts, attr_val_counts):
+def parse_xml(tree, article_counts, level_counts, tag_counts, attr_val_counts):
     level_parsed = set()
     tag_parsed = set()
     attr_val_parsed = set()
 
     try:
         root = tree.getroot()
+        # Need to have the above level too.
+        article_type = root.find('.//article')
+        article_counts[article_type.attrib['article-type']] += 1
         for child in root.findall('.//article/*'):
             if child.tag not in level_parsed:
                 level_counts[child.tag] += 1
@@ -44,7 +47,7 @@ def parse_xml(tree, level_counts, tag_counts, attr_val_counts):
                         attr_val_parsed.add(attr_val_key)
     except AttributeError:
         pass
-    return level_counts, tag_counts, attr_val_counts
+    return article_counts, level_counts, tag_counts, attr_val_counts
 
 
 def open_file(file_location):
@@ -63,6 +66,9 @@ def record_results(file_location, results, header, mode, record_type):
         writer = csv.DictWriter(csv_file, fieldnames=header)
         if mode == 'w':
             writer.writeheader()
+        if record_type == 'article':
+            for article in results:
+                writer.writerow({'article': article, 'count': results[article]})
         if record_type == 'level':
             for level in results:
                 writer.writerow({'level': level, 'count': results[level]})
@@ -90,34 +96,39 @@ def main():
     dl_folder_location = config_all['api_europepmc_params']['article_human_folder']
     sentence_location = config_all['processing_params']['sentence_location']
     interesting_tokens = config_all['processing_params']['token_sentences']
+    list_article_location = config_all['processing_params']['list_article_location']
     list_level_location = config_all['processing_params']['list_level_location']
     list_tags_location = config_all['processing_params']['list_tag_location']
     list_attr_val_location = config_all['processing_params']['list_attr_val_location']
 
     entire_files_to_parse = list(Path(dl_folder_location).glob("*.xml"))
 
+    header_article = ['article', 'count']
     header_level = ['level', 'count']
     header_tag = ['level', 'tag', 'count']
     header_attr_val = ['level', 'tag', 'attr', 'attr_val', 'count']
     count = 0
     mode = 'w'
 
+    article_counts = defaultdict(int)
     level_counts = defaultdict(int)
     tag_counts = defaultdict(lambda: defaultdict(int))
     attr_val_counts = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(int))))
     for file_ in tqdm(entire_files_to_parse):
         xml = open_file(file_)
         tree = get_tree_xml(xml)
-        current_levels, current_tags, current_attr_val = parse_xml(tree, level_counts, tag_counts, attr_val_counts)
+        article_counts, level_counts, tag_counts, attr_val_counts = parse_xml(tree, article_counts, level_counts, tag_counts, attr_val_counts)
         count += 1
-        if count % 1000 == 0:
+        if count % 5000 == 0:
 
-            record_results(list_level_location, current_levels, header_level, mode, 'level')
-            record_results(list_tags_location, current_tags, header_tag, mode, 'tag')
-            record_results(list_attr_val_location, current_attr_val, header_attr_val, mode, 'attr')
-    record_results(list_level_location, current_levels, header_level, mode, 'level')
-    record_results(list_tags_location, current_tags, header_tag, mode, 'tag')
-    record_results(list_attr_val_location, current_attr_val, header_attr_val, mode, 'attr')
+            record_results(list_article_location, article_counts, header_article, mode, 'article')
+            record_results(list_level_location, level_counts, header_level, mode, 'level')
+            record_results(list_tags_location, tag_counts, header_tag, mode, 'tag')
+            record_results(list_attr_val_location, attr_val_counts, header_attr_val, mode, 'attr')
+    record_results(list_article_location, article_counts, header_article, mode, 'article')
+    record_results(list_level_location, level_counts, header_level, mode, 'level')
+    record_results(list_tags_location, tag_counts, header_tag, mode, 'tag')
+    record_results(list_attr_val_location, attr_val_counts, header_attr_val, mode, 'attr')
 
 
 if __name__ == "__main__":
--- pipeline/extract_sentences.py
+++ pipeline/extract_sentences.py
@@ -1,22 +1,27 @@
-import json
+import os
+import yaml
 from nltk.tokenize import sent_tokenize, word_tokenize
 # nltk.download('punkt') # uncomment the first time you run the script
 import csv
-import pathlib
-import pandas as pd
+from pathlib import Path
 from tqdm import tqdm
-import argparse
+from utils.extract_info import get_content
+
+config_path = os.path.join(os.path.dirname(
+    __file__), '../config', 'config.yaml')
+config_all = yaml.safe_load(open(config_path))
 
 
 def find_candidate_sentences(text, relevant_tokens, get_null_sentences=False):
     sentences = sent_tokenize((text))
     interest_sentences = []
+    # token_found = set()
     for sent in sentences:
         candidate = False
         window = 3
         tokenized = word_tokenize(sent)
         for i, token in enumerate(tokenized):
-            if token.lower() in relevant_tokens:
+            if token.lower().strip() in relevant_tokens:
                 check_tokens = tokenized[i - window:i + window]
                 for t in check_tokens:
                     if t.isnumeric():
@@ -30,60 +35,42 @@ def find_candidate_sentences(text, relevant_tokens, get_null_sentences=False):
 
     return interest_sentences
 
-def parsing_arguments(parser):
-    parser.add_argument("--data_path", type=str, default="data/articles_human/",
-                        help='Tasks to evaluate')
-    parser.add_argument("--out", type=str, default='data/candidate_sentences.csv',
-                        help='Tasks to evaluate')
-    return parser
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser = parsing_arguments(parser)
-    args = parser.parse_args()
-    print(args)
+    dl_folder_location = config_all['api_europepmc_params']['article_human_folder']
+    sentence_file_location = config_all['processing_params']['sentence_location']
+    interesting_tokens = config_all['processing_params']['token_sentences']
 
+    interesting_fields = ['METHODS', 'SUBJECTS']
+
+    header = ["PMCID", "tokenized_text"]
+
+    open(sentence_file_location, 'w')
+
+    entire_files_to_parse = list(Path(dl_folder_location).glob("*.xml"))
     count_articles = 0
-    interesting_tokens = ['man', 'woman', 'male', 'female', 'men', 'women', 'males', 'females']
-    header = ["PMCID", "tokenized_METHODS", "keywords"]
+    ext_sent = 0
+    for file_ in tqdm(entire_files_to_parse):
+        pmcid = file_.stem
+        method_text = get_content(file_, interesting_fields, article_type='research-article')
 
-    open(args.out, 'w')
+        if method_text:
+            count_articles += 1
+            sentences = find_candidate_sentences(method_text, interesting_tokens, get_null_sentences=True)
+            if sentences:
+                ext_sent += 1
 
-    for file in tqdm(pathlib.Path(args.data_path).glob("*.jsonl")):
-        pmcid = file.stem
+                with open(sentence_file_location, 'a') as o:
+                    writer = csv.writer(o)
+                    if count_articles == 1:
+                        writer.writerow(header)
 
-
-        with open(file, 'r') as o:
-            data = json.load(o)
-            json_df = pd.read_json(file)
-
-        count_articles += 1
-
-        # filter JSON files that have no rows
-        if json_df.empty:
-            pass
-        if json_df.shape[0] > 1:
-            json_df_clean = json_df.loc[json_df.astype(str).drop_duplicates().index]
-            json_df_clean = json_df_clean.iloc[0]  # this is just a workaround
-            # json_df_clean.drop("TABLE", inplace=True)
-
-        sentences = find_candidate_sentences(data["METHODS"], interesting_tokens, get_null_sentences=True)
-        if sentences:
-
-            with open(args.out, 'a') as o:
-                writer = csv.writer(o)
-                if count_articles == 1:
-                    writer.writerow(header)
-
-                for sentence in sentences:
-                    # filename = os.path.basename(file[:-6]).split("archive_articles_tagged")[1]
-                    # pmcid
-
-                    try:  # Only when "full" df is used
-                        writer.writerow([pmcid, sentence, json_df_clean["KEYWORDS"]])
-                    except:
-                        writer.writerow([pmcid, sentence, "None"])
-            o.close()
+                    for sentence in sentences:
+                        writer.writerow([pmcid, sentence])
+                        # writer.writerow([pmcid, sentence, "None"])
+                o.close()
+    print(f'total of article with methods and subject extracted: {count_articles}')
+    print(f'total of article with extracted sentences: {ext_sent}')
 
 
 if __name__ == "__main__":
