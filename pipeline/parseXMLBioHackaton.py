52773104baf54a55b617d0b29763e46e1ee00896
Olivier Philippe <olivier.philippe@gmail.com>
2023-10-03 17:31:17 +0200

    Modify the parseXMLBiohack to run // and being able to rerun if failed

--- pipeline/parseXMLBioHackaton.py
+++ pipeline/parseXMLBioHackaton.py
@@ -1,171 +1,230 @@
 # Import libraries
+import concurrent.futures
 import requests
 import xml.etree.ElementTree as ET
 import sqlite3
-import gzip
-import io
-import argparse
-import csv
+import sys
+import os
+import yaml
+import logging
+import random
+from tqdm import tqdm
 
-# Argument parser
-parser = argparse.ArgumentParser(description='This program extracts sections and metadata of XML article from EuropePMC')
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+logger = logging.getLogger(__name__)
 
 
-parser.add_argument("-d", "--database", help="Required. Database Name where the data will be stored. Not possible to update the database. It should have '.db' sufix",
-required=True)
+config_path = os.path.join(os.path.dirname(__file__), "../config", "config.yaml")
+config_all = yaml.safe_load(open(config_path))
 
-parser.add_argument("-i", "--input", help="Required. File with all the PMID that will be inputed to the API. If you write 'all', this script will parse all OpenAccess XML files",
-required=True)
-
-args = parser.parse_args()
-
-# Input PMID file, if not we will parse all available PMID publications of EuropePMC
-# 
-
-# # Name of the database
-DB_FILE = args.database
-
-# # Connect to the SQLite database
-# # If name not found, it will create a new database
-conn = sqlite3.connect(DB_FILE)
-c = conn.cursor()
 
 # Never worked in supplementary material, don't know what to expect
-def retrieveSupplementary(root):
-    for supplementary in root.iter('supplementary-material'):
-        return str(ET.tostring(supplementary)).replace('"',"'")
+# Ok we comment it out as we don't need it
+# I remove any ref of it in the createDatabase(), commitToDatabase(), apiSearch() [Olivier]
+# def retrieveSupplementary(root):
+#     for supplementary in root.iter("supplementary-material"):
+#         return str(ET.tostring(supplementary)).replace('"', "'")
+
 
 def retrieveMetadata(root):
-    
     dictMetadata = {
-        'issn':{'ppub':'', 'epub':''},
-     'journalTitle':'',
-      'publisherName':''
+        "issn": {"ppub": "", "epub": ""},
+        "journalTitle": "",
+        "publisherName": "",
     }
 
-    for front in root.iter('front'):
-        for journalId in front.iter('issn'):
-            if "ppub" in journalId.attrib['pub-type']:
-                dictMetadata['issn']['ppub'] = ''.join(journalId.itertext())
-            if "epub" in journalId.attrib['pub-type']:
-                dictMetadata['issn']['epub'] = ''.join(journalId.itertext())
-        for journalTitle in front.iter('journal-title'):
-            dictMetadata['journalTitle'] = ''.join(journalTitle.itertext())
-        for publisher in front.iter('publisher-name'):
-            dictMetadata['publisherName'] = ''.join(publisher.itertext())
+    for front in root.iter("front"):
+        for journalId in front.iter("issn"):
+            if "ppub" in journalId.attrib["pub-type"]:
+                dictMetadata["issn"]["ppub"] = "".join(journalId.itertext())
+            if "epub" in journalId.attrib["pub-type"]:
+                dictMetadata["issn"]["epub"] = "".join(journalId.itertext())
+        for journalTitle in front.iter("journal-title"):
+            dictMetadata["journalTitle"] = "".join(journalTitle.itertext())
+        for publisher in front.iter("publisher-name"):
+            dictMetadata["publisherName"] = "".join(publisher.itertext())
     return dictMetadata
+
 
 # Retrieve the text for each section
 def retrieveSections(root):
-
-    dictSection = {'Intro': '', 'Method': '', 'Result': '', 'Discussion': ''}
-    for body in root.iter('body'):
-        for child in body.iter('sec'):
+    dictSection = {"Intro": "", "Method": "", "Result": "", "Discussion": ""}
+    for body in root.iter("body"):
+        for child in body.iter("sec"):
             if "sec-type" in child.attrib:
-                for section in dictSection.keys(): # For each section in dictSection
+                for section in dictSection.keys():  # For each section in dictSection
                     # Sections inside sec-type are written in the following form:
                     # intro, methods, results, discussion
                     if section.lower() in child.attrib["sec-type"].lower():
-                        dictSection[section] = ''.join(child.itertext()).replace('"',"'")# Text without tags
+                        dictSection[section] = "".join(child.itertext()).replace(
+                            '"', "'"
+                        )  # Text without tags
                         # dictSection[section] = ET.tostring(child) # Text with tags
             if child[0].text:
-                for section, sectionData in dictSection.items():                
+                for section, sectionData in dictSection.items():
                     if sectionData:
                         continue
                     if section.lower() in child[0].text.lower():
-                        dictSection[section] = ''.join(child.itertext()).replace('"',"'")# Text without tags
+                        dictSection[section] = "".join(child.itertext()).replace(
+                            '"', "'"
+                        )  # Text without tags
                         # dictSection[section] = ET.tostring(child) # Text with tags
     return dictSection
 
-def commitToDatabase(pmcid, dictSection, dictMetadata, supMaterial):
-    c.execute(f'''INSERT OR IGNORE INTO Main
-    values ("{pmcid}", "{dictSection["Intro"]}", "{dictSection["Method"]}",
-    "{dictSection["Result"]}", "{dictSection["Discussion"]}", "{supMaterial}",
-     "{dictMetadata["issn"]["ppub"]}", "{dictMetadata["issn"]["epub"]}",
-     "{dictMetadata["journalTitle"]}", "{dictMetadata["publisherName"]}")''')
-    conn.commit()
 
-def createDatabase():
-    c.execute('''DROP TABLE IF EXISTS Main''')
-    c.execute('''CREATE TABLE IF NOT EXISTS "Main" (
+def createDatabase(conn):
+
+    c = conn.cursor()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS "Main" (
 	            "pmcid"	TEXT NOT NULL,
-	            "Introduction"	TEXT,
+                "api_response" TEXT NOT NULL,
+	            "Introduction" TEXT,
 	            "Methods" TEXT,
 	            "Result" TEXT,
 	            "Discussion" TEXT,
-                "SupMaterial" TEXT,
-	            "ISSN PPUB" TEXT,
-                "ISSN EPUB" TEXT,
-                "JournalTitle" TEXT,
-                "PublisherName" TEXT,
+	            "issn_ppub" TEXT,
+                "issn_epub" TEXT,
+                "journalTitle" TEXT,
+                "publisherName" TEXT,
 	            PRIMARY KEY("pmcid")
-            )''')
+            )"""
+    )
+    conn.commit()
+
+
+def retrieve_pmcids(conn):
+
+    c = conn.cursor()
+    # Execute a SELECT query to retrieve the pmcid values
+    c.execute("SELECT pmcid FROM Main")
+
+    # Fetch all the pmcid values and store them in a list
+    pmcid_list = [row[0] for row in c.fetchall()]
+    c.close()
+    return pmcid_list
+
 
 def apiSearch(pmcid):
-    req = f'https://www.ebi.ac.uk/europepmc/webservices/rest/{pmcid}/fullTextXML'
-    print(req)
+    req = f"https://www.ebi.ac.uk/europepmc/webservices/rest/{pmcid}/fullTextXML"
     r = requests.get(req)
     if not r:
-        return
+        api_response = False
+        return pmcid, api_response, dict(), dict()
+    else:
+        api_response = True
     root = ET.fromstring(r.content)
-    if not root.findall('body'):
-        return
-    dictSection = retrieveSections(root)
-    dictMetadata = retrieveMetadata(root)
-    supMaterial = retrieveSupplementary(root)
-
-    methods_dict = dict()
-    methods_dict["pmcid"] = pmcid
-    methods_dict["methods"] = dictSection["Method"]
+    if root.findall("body"):
+        dictSection = retrieveSections(root)
+        dictMetadata = retrieveMetadata(root)
+    else:
+        dictSection = dict()
+        dictMetadata = dict()
+    return pmcid, api_response, dictSection, dictMetadata
 
 
-    commitToDatabase(pmcid, dictSection, dictMetadata, supMaterial)
+# FIXME: dirty way to check if values are present, should be done better and
+# Remove the hardcoded values to put in config file
 
-    return methods_dict
+
+def format_values(pmcid, api_response, dictSection, dictMetadata):
+    values_to_record = []
+    values_to_record.append(pmcid)
+    values_to_record.append(api_response)
+    sections_values = ["Intro", "Method", "Result", "Discussion"]
+    for val in sections_values:
+        values_to_record.append(dictSection.get(val, None))
+    issn_values = ["ppub", "epub"]
+    try:
+        for val in issn_values:
+            values_to_record.append(dictMetadata["issn"].get(val, None))
+    # In case the issn key is not present, add as much none as the values needed
+    except KeyError:
+        for _ in range(len(issn_values)):
+            values_to_record.append(None)
+    metadata_values = ["journlaTitle", "publisherName"]
+    for val in metadata_values:
+        values_to_record.append(dictMetadata.get(val, None))
+    return values_to_record
+
+
+def commitToDatabase(conn, values_to_record):
+
+    c = conn.cursor()
+    sql_query = f"""
+    INSERT OR IGNORE INTO Main (pmcid, api_response, Introduction, Methods, Result, Discussion, issn_ppub, issn_epub, journalTitle, publisherName) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) """
+    c.execute(sql_query, values_to_record)
+    conn.commit()
+    c.close()
+
+
+def get_list_to_dl(pmcid_location, list_parsed):
+    def get_list_humans(file_location):
+        try:
+            with open(file_location, "r") as f:
+                for l in f:
+                    yield l.split(",")[0].rstrip()
+        except FileNotFoundError:
+            raise
+
+    list_humans = set(get_list_humans(pmcid_location))
+    logger.info(f"Len of complete list of human pmcid: {len(list_humans)}")
+    logger.info(f"Len of already done species: {len(list_parsed)}")
+
+    ids_to_dl = list(list_humans.difference(set(list_parsed)))
+    logger.info(f"Len of pmcid to parse: {len(ids_to_dl)}")
+    # Randomize the list to avoid downloading only the first articles
+    random.shuffle(ids_to_dl)
+    return ids_to_dl
 
 
 def main():
+    pmcid_human_file = config_all["api_europepmc_params"]["pmcid_human_file"]
+    # # Name of the database
+    DB_FILE = config_all["api_europepmc_params"]["db_info_articles"]
+
+    # # Connect to the SQLite database
+    # # If name not found, it will create a new database
+    conn = sqlite3.connect(DB_FILE)
+    createDatabase(conn)
+    # Parse the db and get the already dl pmcids list and remove them from
+    # The original list
+    pmcid_db_dl = retrieve_pmcids(conn)
+    ids_to_dl = get_list_to_dl(pmcid_human_file, pmcid_db_dl)
+    logger.info(f"Got the list of pmcids to dl: {len(ids_to_dl)}")
+
+    try:
+        futures = []
+        executor = concurrent.futures.ThreadPoolExecutor()
+        logging.info("Getting the information from the list of pmcid")
+        futures = [executor.submit(apiSearch, pmcid) for pmcid in ids_to_dl]
+
+        logger.info("Process started. Getting results")
+        pbar = tqdm(total=len(ids_to_dl))  # Init pbar
+        for future in concurrent.futures.as_completed(futures):
+            pmcid, api_response, dictSection, dictMetadata = future.result()
+            values_to_record = format_values(
+                pmcid, api_response, dictSection, dictMetadata
+            )
+            commitToDatabase(conn, values_to_record)
+            pbar.update(n=1)
+            exception = future.exception()
+            if exception:
+                logger.error(
+                    "Got an exception: {exception}.\nClose the db connection and exit."
+                )
+                conn.close()
+                raise (exception)
+        conn.close()
+    except Exception as e:
+        logger.error(
+            f"An unexpected exception occurred: {e}. Closing the db connection and exiting."
+        )
+        conn.close()
+        raise e  # Re-raise the exception for further handling if needed
 
 
-    
-    dummyCounter = 0
-    createDatabase()
-
-    if args.input == "all":
-        OAUrl = requests.get("https://europepmc.org/ftp/oa/pmcid.txt.gz")
-        gzFile = OAUrl.content
-        f = io.BytesIO(gzFile)
-        with gzip.GzipFile(fileobj=f) as OAFiles:
-            for OAFile in OAFiles:
-                dummyCounter += 1
-                dict_with_methods = apiSearch(str(OAFile[:-1],"utf-8"))
-                writer.writerow(dict_with_methods)
-
-
-    else:
-
-        with open(args.input, 'r') as f:
-
-            columns = ["pmcid", "methods"]
-
-            with open("./output/methods_subset_5000_2906_ok.csv", "a") as csvfile: 
-                writer = csv.DictWriter(csvfile, columns)
-                writer.writeheader()
-
-                for listFiles in f:
-                    dummyCounter += 1
-                    listFiles = listFiles.strip()
-                    dict_with_methods = apiSearch(listFiles)
-                    writer.writerow(dict_with_methods)
-            
-            csvfile.close()
-
-
-    print(dummyCounter)
-
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
