16c955a337a4e81551ee406ba5796df63532f22a
Claire Furtick <claire.furtick@bsc.es>
2023-12-13 11:52:35 +0100

    updates to script for bh2023

--- pipeline/get_sex_bias.py
+++ pipeline/get_sex_bias.py
@@ -11,7 +11,8 @@ import yaml
 import nltk
 from nltk.tokenize import sent_tokenize
 import concurrent.futures
-
+import re
+from word2number import w2n
 
 def parsing_arguments(parser):
     parser.add_argument("--model", type=str, default='output/bert-base-uncased-en/sbe.py_8_0.00005_date_22-11-10_time_14-55-26',
@@ -20,26 +21,41 @@ def parsing_arguments(parser):
  
 # Method for getting database entries that still need to be run through the model
 def get_entries(conn, cur):
-    print("Getting entries...")
-    SQL_QUERY = 'SELECT CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END as IsEmpty FROM Results;'
-    cur.execute(SQL_QUERY)
-    result = cur.fetchone()
+    # SQL_QUERY = 'SELECT CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END as IsEmpty FROM Results;'
+    # cur.execute(SQL_QUERY)
+    # result = cur.fetchone()
 
-    # If results table is empty, start with all records
-    if result[0]:
-        print("Results is empty, start from the beginning.")
-        SQL_QUERY = 'SELECT sections.pmcid, sections.METHODS FROM sections;'
-        cur.execute(SQL_QUERY)
-    else:
-        print("Results is not empty, fetch methods still needing to be processed.")
-        SQL_QUERY = 'SELECT sections.pmcid, sections.METHODS FROM sections LEFT JOIN status ON sections.pmcid = status.pmcid WHERE status.model_results IS NULL;'
-        cur.execute(SQL_QUERY)
+    # # If results table is empty, start with all records
+    # if result[0]:
+    #     print("Results is empty, start from the beginning.")
+    #     SQL_QUERY = 'SELECT sections.pmcid, sections.METHODS FROM sections;'
+    #     cur.execute(SQL_QUERY)
+    # else:
+    #     print("Results is not empty, fetch methods still needing to be processed.")
+    #     SQL_QUERY = 'SELECT sections.pmcid, sections.METHODS FROM sections LEFT JOIN status ON sections.pmcid = status.pmcid WHERE status.model_results IS NULL;'
+    #     cur.execute(SQL_QUERY)
+    # return cur.fetchall()
+
+    # FOR TESTING
+    # SQL_QUERY = 'SELECT sections.pmcid, sections.METHODS FROM sections LIMIT 10'
+    # cur.execute(SQL_QUERY)
+    # return cur.fetchall()
+
+    # For BH2023
+    pmcid_list = []
+
+    with open('pmcid_list.txt', 'r') as file:
+        for line in file:
+            pmcid_list.append(str(line.strip()))
+
+    SQL_QUERY = f"SELECT sections.pmcid, sections.METHODS FROM sections WHERE sections.pmcid IN ({', '.join(['?' for _ in pmcid_list])}) LIMIT 5"
+    result = cur.execute(SQL_QUERY, pmcid_list)
     return cur.fetchall()
 
 # Create table to record model results in (pmcid, n_fem, n_male, per_fem, perc_male, sample)
 def create_results_table(conn, cur):
     cur.execute(
-        """CREATE TABLE IF NOT EXISTS "Results" (
+        """CREATE TABLE IF NOT EXISTS "model_output" (
                 "pmcid"	TEXT NOT NULL,
                 "sentence_index"	INTEGER,
                 "n_fem"	TEXT,
@@ -55,9 +71,8 @@ def create_results_table(conn, cur):
 # Add new row to results  
 def add_result(conn, cur, pmcid, results):
 
-    SQL_QUERY = "INSERT INTO Results (pmcid, sentence_index, n_male, n_fem, perc_male, perc_fem, sample) VALUES  (?, ?, ?, ?, ?, ?, ?)"
-    for row in results:
-        cur.execute(SQL_QUERY, row)
+    SQL_QUERY = "INSERT INTO model_output (pmcid, sentence_index, n_male, n_fem, perc_male, perc_fem, sample) VALUES  (?, ?, ?, ?, ?, ?, ?)"
+    cur.execute(SQL_QUERY, tuple(results))
     update_status(conn, cur, pmcid)
 
 def update_status(conn, cur, pmcid):
@@ -65,29 +80,78 @@ def update_status(conn, cur, pmcid):
     cur.execute(SQL_QUERY, (pmcid,))
     conn.commit()
 
+def process_list(input_list):
+    result_list = []
+    for item in input_list:
+        try:
+            number = int(item)
+            result_list.append(number)
+        except ValueError:
+            # If it is not a valid integer, try a float
+            try:
+                number = float(item)
+                result_list.append(number)
+            except ValueError:
+                # If it's not an integer or float, try word2number
+                try:
+                    number = w2n.word_to_num(item)
+                    result_list.append(number)
+                except ValueError:
+                    # If word2number also fails, discard the item
+                    pass
+    
+    return result_list
+
 def run_model_on_entry(row, nlp):
     pmcid, methods = row
     results = []
     # Split methods section into sentences
     if methods is not None:
         sentences = sent_tokenize(methods)
-        # Establish array of tuples of results per sentence
-        #results = []
-        index = 0
-        # Loop through each sentence
         for sentence in sentences:
-            # Truncate the sentence to the maximum length of 512 tokens
             if len(sentence) > 512:
                 sentence = sentence[:512]
-            annotations = nlp(sentence)
-            dict = {'pmcid' : pmcid, 'sentence_index' : index, 'n_male' : None, 'n_fem' : None, 'perc_male' : None, 'perc_fem' : None, 'sample' : None}
-            for annotation in annotations:
-                dict[annotation["entity"]] = json.dumps(annotation["word"])
-            # If there were results from the model, add to results array
-            if not ((dict['n_male'] is None) and (dict['n_male'] is None) and (dict['n_fem'] is None) and (dict['perc_male'] is None) and (dict['perc_fem'] is None) and (dict['sample'] is None)):
-                values = list(dict.values())
-                results.append(tuple(values))
-            index += 1
+
+        annotations = nlp(sentences)
+        
+        sentence_index = []
+        n_male = []
+        n_fem = []
+        perc_male = []
+        perc_fem = []
+        sample = []
+
+        sent_index = 0
+
+        for list in annotations:
+            if list:
+                dict = {'pmcid' : pmcid, 'sentence_index' : sent_index, 'n_male' : [], 'n_fem' : [], 'perc_male' : [], 'perc_fem' : [], 'sample' : []}
+                list_index = 0
+                for annotation in list:
+                    # if the token we are looking at should be appended to the previous token
+                    if (list_index > 0) and (annotation["entity"] == list[list_index-1]["entity"]) and (annotation['word'].startswith("##")) and (list[list_index-1]["end"] == annotation["start"]):
+                        # Get the last entry added to the list for this entity in the dictionary (this is what we need to append to)
+                        print("Found token to append")
+                        prev = dict[annotation["entity"]][-1]
+                        # Remove hashtags
+                        add = annotation["word"].replace("#", "")
+                        # Append current number to previous numbers
+                        dict[annotation["entity"]][-1] = prev + add
+                    else:
+                        dict[annotation["entity"]].append(annotation["word"])
+                    list_index += 1
+
+                #TO DO: clean values before adding? Clean words into ints and turn list of strings into list of integers
+                sentence_index.append(sent_index)
+                n_male.append(process_list(dict["n_male"]))
+                n_fem.append(process_list(dict["n_fem"]))
+                perc_male.append(process_list(dict["perc_male"]))
+                perc_fem.append(process_list(dict["perc_fem"]))
+                sample.append(process_list(dict["sample"]))
+            sent_index += 1
+
+        results = [pmcid, json.dumps(sentence_index), json.dumps(n_male), json.dumps(n_fem), json.dumps(perc_male), json.dumps(perc_fem), json.dumps(sample)]
+
     return pmcid, results
 
 def main():
@@ -102,15 +166,17 @@ def main():
     config_all = yaml.safe_load(open(config_path))
 
     # Connect to the SQLite database
+    print('Connecting to DB...')
     DB_FILE = config_all["api_europepmc_params"]["db_info_articles"]
     conn = sqlite3.connect(DB_FILE)
     cur = conn.cursor()
 
     # Create results table (if not already created)
     create_results_table(conn, cur)
-    print("Created results table")
+    print("Created new results table")
 
     # Get entries from db that need to be run through the model
+    print("Getting entries...")
     entries = get_entries(conn, cur)
     print("Got entries to be processed: ", len(entries))
 
